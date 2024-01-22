431b84b986343a6e9e60131fc3ea4d5751dbe023
PinkFloyd1213 <31860086+PinkFloyd1213@users.noreply.github.com>
2024-01-22 15:54:42 +0100

    Update invite_url

--- modules/invite_role_bot/invite_role_bot_config.py
+++ modules/invite_role_bot/invite_role_bot_config.py
@@ -1,7 +1,7 @@
 from modubot import PropertyDict
 
 class HelpConfig(PropertyDict):
-    invite_url: str = "https://discord.com/oauth2/authorize?client_id=1196435760279195750&scope=bot&permissions=2415920129"
+    invite_url: str = "https://discord.com/oauth2/authorize?client_id=1196435760279195750&scope=bot&permissions=268438561"
     github_url: str = "https://github.com/PinkFloyd1213/InviteGuard"
     privacy_policy_url: str = "https://gist.github.com/PinkFloyd1213/7ffd080945f57048a062d8767e2ed008"
     credit_url: str = "https://github.com/wow13524/invite-role-bot"
